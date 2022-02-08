dd8a002c440447f9957d3d72de7d92d4489c1c4d
yoav-ro <89576513+yoav-ro@users.noreply.github.com>
2022-02-08 16:25:15 +0200

    getting pastes

--- backend/utils/requests.js
+++ backend/utils/requests.js
@@ -1,20 +1,51 @@
-const axios=require("axios");
-const cheerio= require("cheerio");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
+const onionURL = "http://strongerw2ise74v3duebgsvug4mehyhlpa7f6kfwnas7zofs3kov7yd.onion/all"
 
-const onionURL= "http://strongerw2ise74v3duebgsvug4mehyhlpa7f6kfwnas7zofs3kov7yd.onion/all"
-
-async function getHtmlFeed(){
+async function getHtmlPage(pageURL) {
     try {
-        const res =await axios.get(onionURL, {
-            proxy:{
+        const res = await axios.get(pageURL, {
+            proxy: {
                 host: "localhost",
                 port: "8118"
             }
         });
-        console.log(res.data)
+        return res.data;
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
-getHtmlFeed();
+
+async function getPastesData(onionURL) {
+    const pastesArray = [];
+    const pastesHTML = await getHtmlPage(onionURL)
+    const $ = await cheerio.load(pastesHTML);
+
+    $(".col-sm-12").each((index, parentElem) => {
+        const title = $(parentElem).find("div.pre-info.pre-header div.row div.col-sm-5 h4").text()
+        const footerStr = $(parentElem).find("div.pre-info.pre-footer div.row div.col-sm-6").text();
+        const author = footerStr.split(" ")[2];
+        const date = getDateString(footerStr);
+        const content = $(parentElem).find("div.well.well-sm.well-white.pre div.text ol").text();
+
+        const pasteObj = {
+            title: title,
+            author: author,
+            date: date,
+            content: content
+        }
+        pastesArray.push(pasteObj)
+    })
+    return pastesArray.slice(1, pastesArray.length - 1);
+}
+
+function getDateString(footerStr) {
+    const footerArr = footerStr.split(" ");
+    const dateStr = footerArr[4] + " " + footerArr[5] + " " + footerArr[6] + " " + footerArr[7];
+    return new Date(dateStr);
+}
+
+getPastesData(onionURL).then((value) => {
+    console.log(value)
+});
